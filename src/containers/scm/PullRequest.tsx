7df594a5ad662c92e1d84ee91c611b6ca4f444bb
unknown <smutyala@equiwiz.com>
2024-05-03 12:24:55 +0530

    user persona fill form created

--- src/containers/scm/PullRequest.tsx
+++ src/containers/scm/PullRequest.tsx
@@ -3,6 +3,8 @@ import React, { HTMLProps } from 'react';
 
 export const PullRequest = React.forwardRef<HTMLDivElement, HTMLProps<HTMLDivElement>>(({ ...props }, ref) => {
       return (
-            <div {...props} ref={ref} ></div>
+            <div {...props} ref={ref} >
+                  
+            </div>
       )
 })
