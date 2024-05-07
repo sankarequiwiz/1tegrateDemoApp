42afd5eea4a832a627b45363b4f403f766fb14b7
Gunaseelan Kumar <guKumar@clearsense.com>
2024-05-07 10:12:38 +0530

    add: demo app full flow

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -2,4 +2,4 @@ import io from 'socket.io-client';
 
 const url = import.meta.env.VITE_ITEGRATE_BASE_URL;
 
-export const socket = io(url, { autoConnect: true });
+export const socket = io(url, { autoConnect: false });
