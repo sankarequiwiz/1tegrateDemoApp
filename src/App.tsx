4b1bd50d8fc2821911b4e3a60e9b6b3b47f07097
Gunaseelan Kumar <guKumar@clearsense.com>
2024-05-15 18:28:06 +0530

    add: utility fixed

--- src/App.tsx
+++ src/App.tsx
@@ -16,32 +16,31 @@ import { Header } from './components/Header';
 import { Events } from './containers/events';
 
 function Root() {
-  const test = '';
-  return (
-    <BrowserRouter>
-      <WatchEventsProvider>
-        <StepProvider>
-          <AppProvider>
-            <ConfigProvider theme={{ token: { borderRadius: 2 } }}>
-              <App />
-            </ConfigProvider>
-          </AppProvider>
-        </StepProvider>
-      </WatchEventsProvider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<WatchEventsProvider>
+				<StepProvider>
+					<AppProvider>
+						<ConfigProvider theme={{ token: { borderRadius: 2 } }}>
+							<App />
+						</ConfigProvider>
+					</AppProvider>
+				</StepProvider>
+			</WatchEventsProvider>
+		</BrowserRouter>
+	);
 }
 
 function App() {
-  return (
-    <Fragment>
-      <Header style={{ position: 'fixed', top: 0, width: '100%', zIndex: 2 }} />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/events" element={<Events />} />
-      </Routes>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Header style={{ position: 'fixed', top: 0, width: '100%', zIndex: 2 }} />
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/events" element={<Events />} />
+			</Routes>
+		</Fragment>
+	);
 }
 
 export default Root;
