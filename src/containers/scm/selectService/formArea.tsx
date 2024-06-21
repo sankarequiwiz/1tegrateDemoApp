1f99a3e12dcfa0673d5a316104ea2457a39e8e8b
Gunaseelan Kumar <guKumar@clearsense.com>
2024-06-21 15:47:15 +0530

    add: table structure finished

--- src/containers/scm/selectService/formArea.tsx
+++ src/containers/scm/selectService/formArea.tsx
@@ -44,28 +44,6 @@ export const FormArea = React.forwardRef<
       return 'not_set';
    }, [fields]);
 
-   const integrationPayloadKey = {
-      API_KEY: {
-         value: 'apiKey'
-      },
-      USERNAME: {
-         value: 'username'
-      },
-      PASSWORD: {
-         value: 'password'
-      },
-      DOMAIN: {
-         value: 'domain'
-      },
-      CLIENT_ID: {
-         value: "clientId"
-      },
-      CLIENT_SECRET :{
-         value : "clientSecret"
-      }
-
-   }
-
    const onIntegrate = (callback) => {
       if (organization) {
          form
