02f6fa1d99a3245e4744373edb19bf02ea23cfab
unknown <smutyala@equiwiz.com>
2024-07-12 09:32:30 +0530

    chnages done

--- src/containers/pcr/Tag.tsx
+++ src/containers/pcr/Tag.tsx
@@ -47,90 +47,11 @@ export const Tag = React.forwardRef<
     }
   };
 
-  const downloadHandler = async () => {
-    setDownloading(true);
-    try {
-      await API.services.repositoryDownload(
-        {
-          orgId: selectedOrganization,
-          repoId: selectedRepo,
-        },
-        getHeaders()
-      );
-      messageApi.success('Repository downloaded successfully.');
-    } catch (error) {
-      console.error(error);
-      messageApi.error('Failed to download the repository');
-    } finally {
-      setDownloading(false);
-    }
-  };
-
-  const handleCreateWatch = async () => {
-    setLoading(true);
-    const fullBodySelected = tags.find(
-      (item) => item?.id?.toString() === selectedRepo
-    );
-    const payload: any = {
-      name: `web-gateway-service-${fullBodySelected?.fullName}`,
-      description: `Watch for ${fullBodySelected.description} repository`,
-      type: 'Webhook',
-      resource: {
-        type: 'PCR_REPOSITORY',
-        repository: {
-          id: selectedRepo,
-        },
-        organization: {
-          id: selectedOrganization,
-        },
-      },
-    };
-    try {
-      await API.services.createWatch(payload, integration.id);
-      messageApi.success({ content: 'Watch created successfully' });
-    } catch (error) {
-      console.log(error);
-      const errorMessage = error?.response?.data, status = error?.response?.status;
-      messageApi.error({ content: handleError(errorMessage, status) });
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const isWatchEnabled = useMemo(() => {
-    const watch = new utils.watch.Watch(domain);
-    return watch.isAvailable({ level: 'repository' })
-  }, [domain]);
-
-  const items: MenuProps['items'] = [
-    {
-      label: "Create Watch",
-      key: '0',
-      style: { display: isWatchEnabled ? 'block' : 'none' },
-      icon: <EyeOutlined />,
-      onClick: (e) => {
-        e.domEvent.stopPropagation();
-        handleCreateWatch()
-      }
-    },
-    {
-      label: 'Download',
-      key: '1',
-      icon: <DownloadOutlined />,
-      onClick: (e) => {
-        e.domEvent.stopPropagation();
-        downloadHandler();
-      }
-    }
-  ];
 
   React.useEffect(() => {
     getAllTags();
   }, []);
 
-  const onOkProps: ButtonProps = {
-    disabled: !selectedRepo,
-  };
 
   return (
     <Space
@@ -159,19 +80,8 @@ export const Tag = React.forwardRef<
             dataSource={tags}
             loading={loading}
             renderItem={(item) => {
-              const isSelected = selectedArtifact == item.id.toString();
               return (
                 <List.Item
-                  actions={[
-                    isSelected && <Dropdown key={1} menu={{ items }} trigger={['click']}>
-                      <button
-                        onClick={(e) => e.stopPropagation()}
-                        style={{ padding: '.2rem', background: 'transparent', outline: 'none', border: 'none', cursor: 'pointer' }}
-                      >
-                        <EllipsisOutlined />
-                      </button>
-                    </Dropdown>
-                  ]}
                 >
                   <List.Item.Meta
                     title={<a>{item?.name}</a>}
@@ -185,7 +95,7 @@ export const Tag = React.forwardRef<
       <Footer
         onCancel={() => setCurrentStep(current - 1)}
         onSubmit={() => setCurrentStep(0)}
-        onOkProps={onOkProps}
+
       />
     </Space>
   );
